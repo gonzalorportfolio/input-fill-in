1d5a414db7d67e296c413cbddb99b15bad2adfba
gonzalorportfolio <93043629+gonzalorportfolio@users.noreply.github.com>
2023-12-18 19:05:18 +0000

    adding some utils

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,9 @@
 import './style.css'
+import { fetchData, CITIES_LIST, getElement} from './utils/utils.js';
+
+
+const cities = await fetchData(CITIES_LIST);
+ 
 
 const main = ( ) => {
 
---
+++ app/src/utils/utils.js
@@ -0,0 +1,26 @@
+export const getElement = (el) => document.querySelector(el);
+
+export const fetchData = async (url) => {
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
+export const findCities = (toMatch, cities) => {
+    return cities.filter((each) => {
+        const regex = new RegExp(toMatch, 'gi');
+
+        return each.city.match(regex) || each.state.match(regex);
+    });
+}
+
+export const  CITIES_LIST= 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
